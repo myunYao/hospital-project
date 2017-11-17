b525088f94d5bbd9be49ebc0291f1d2709e99500
myunYao <myunYao@163.com>
2017-11-17 15:07:52 +0800

    增添office.js

---
+++ js/office.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+$('#delete-office-msg').click(function(){
+    if(!confirm('你确定要删除该信息吗？')){
+        return ;
+    }
+    var $dc_id = $(this).attr('dc_id');
+    console.log($dc_id)
+    $.ajax({
+        url: officeUrl,
+        type: 'POST',
+        data: {dc_id : $dc_id},
+        success:function (data) {
+            console.log(data)
+            /* body... */
+            if(data.result == 'ok'){
+
+                alert('删除成功！');
+                window.location.reload();  //刷新当前页面
+
+            }else{
+                alert('删除失败！');
+            }
+        }
+    });
+})
