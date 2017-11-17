fdee174dafcb2aa2b2ba9d691ea6d5411f59a910
KyraRain <m17808323224@163.com>
2017-11-17 10:45:29 +0800

    登录操作

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -14,6 +14,7 @@ class Account extends CI_Controller
         $this->load->helper('url');
         $this->load->model('DBModel');
         $this->DBModel->setTable('account');
+
     }
 
     function index()
@@ -21,71 +22,60 @@ class Account extends CI_Controller
         echo '这是默认方法';
         $this->load->view('success');
     }
-
-    /* 添加用户
-     * 至少4个字段"account" "password" "contact" "nickname"
-     * 其它字段见数据表*/
-    function insertAcc()
-    {
-        /*不允许用户名有重复的*/
-        if ($this->DBModel->get(array(
+    function insertAcc(){
+        /*$info = array(
             "account" => $_POST["account"],
-        ))) {
-            echo '已经存在的用户';
-        } else {
-            $info = array(
-                "account" => $_POST["account"],
-                "password" => $_POST["password"],
-                "contact" => $_POST["contact"],
-                "nickname" => $_POST["nickname"]
-            );
-            $res = $this->DBModel->insert($_POST);
-            /*这里添加注册成功或失败跳转的页面*/
-            if ($res){
-                //$this->load->view('某某页面');
-            }else{
-                //$this->load->view('某某页面');
-            }
-        }
-    }
-
-    /*删除信息*/
-    function deleteAcc()
-    {
-        /*根据账户名进行查找删除*/
-        $where = array(
-            "account" => $_POST["account"]
+            "password" => $_POST["password"],
+            "contact" => $_POST["tel"],
+            "nickname" => $_POST["nickname"]
+        );*/
+        $info = array(
+            "account" => 'admin',
+            "password" => 'admin',
+            "contact" => '12346698521',
+            "nickname" => '神经病吧'
         );
-        echo '需要最高级权限';
-//        $this->DBModel->delete($where);
+        $table="account";
+        var_dump($_POST);
+        echo $this->DBModel->insert($info);
     }
-
-    /*修改信息*/
-    function updateAcc()
-    {
-        /*需要修改的数据集合，需要包含account字段*/
-        $info = $_POST;
-        $where = array(
-            "account" => $_POST["account"]
+    function deleteDoc(){
+        $info = array(
+            "name" => $_POST["name"],
+            "sex" => $_POST["sex"],
+            "position" => $_POST["position"],
+            "subject" => $_POST["subject"],
+            "skill" => $_POST["skill"],
+            "room" => $_POST["room"]
         );
-        /*如果修改了密码，将密码进行加密*/
-        if(array_key_exists('password',$_POST)){
-            $info['password'] = md5($_POST['password']);
-        }
-        echo $this->DBModel->update($info,$where);
+        $this->DBModel->delete($info);
     }
-
-    /*获取用户*/
-    function getAcc()
-    {
-        /* 数据请求用post
-         * 需要给出account和password*
-         * 返回账号信息全字段*/
-        $where = array(
-            "account" => $_POST['account'],
-            "password" => md5($_POST['password'])
+    function updateDoc(){
+        $info = array(
+            "name" => $_POST["name"],
+            "sex" => $_POST["sex"],
+            "position" => $_POST["position"],
+            "subject" => $_POST["subject"],
+            "skill" => $_POST["skill"],
+            "room" => $_POST["room"]
         );
+        $this->DBModel->update($info);
+    }
+    function getAcc(){
+        $where = array(
+            "account" =>$_POST['account'],
+            "password"=>md5($_POST['password']),
+        );
+
         $res = $this->DBModel->get($where);
-        echo $res;
+        echo $this->DBModel->db->last_query();
+        var_dump($res[0]["degree"]);
+        if(!count($res)){
+            echo '没有找到数据';
+            return;
+        }
+        else{
+            echo json_encode($res);
+        }
     }
 }
