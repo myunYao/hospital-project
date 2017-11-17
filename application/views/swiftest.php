3cae8c8fa3f3b2182ff029e79d2ea53b9bc26ba2
ShawnCrazy <2273904129@qq.com>
2017-11-17 14:16:04 +0800

    增强了数据操作的安全性，添加了可阅读注释

--- application/views/swiftest.php
+++ application/views/swiftest.php
@@ -13,6 +13,7 @@
 <div class="container">
     <div class="row" id="top">
         <div class="col-md-2 col-md-offset-10">
+            <!--        登录注册-->
             <span><a href="<?php echo site_url('Login')?>">登录 |</a></span>
             <span><a href="<?php echo site_url('Register')?>">注册</a></span>
         </div>
