9de0fd9918e01b2d7b7bd574231201b70d1ade19
myunYao <myunYao@163.com>
2017-11-17 17:23:09 +0800

    addmsg.js

---
+++ js/addmsg.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+$(document).ready(function(){
+    $('#queding').click(function() {
+        var name = $(".name").val();
+        var sex = $(".sex").val();
+        var introduction = $(".introduction").val();
+        var position = $(".position").val();
+        var subject = $(".subject").val();
+        var skill = $(".skill").val();
+        var experience = $(".experience").val();
+        var room = $(".room").val();
+        console.log($('#addmessage').serialize());
+        //提交数据
+        $.ajax({
+            url: addmsgUrl,
+            type: 'POST',
+            data: {name:name,sex:sex,introduction:introduction,position:position,subject:subject,skill:skill,experience:experience,room:room},
+            success:function (data) {
+                /* body... */
+                console.log(data);
+
+                    //alert('操作成功！');
+                   /* window.location.href = tiaozhuanUrl;*/
+
+            }
+        });
+    });
+})
