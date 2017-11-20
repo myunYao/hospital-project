f235004ad0ebfdf73b5728e98663a52c5a53fd72
KyraRain <m17808323224@163.com>
2017-11-20 10:45:33 +0800

    注册时判断用户名是否存在

--- js/register.js
+++ js/register.js
@@ -46,13 +46,18 @@ $(function(){
                 console.log(msg);
             },
             success: function (msg) {  //处理正确时的信息
+
                 console.log(msg);
-                if(msg["degree"]=="normol"){
+
+                if(msg == "exist"){
+                    alert("该用户已存在");
+                }
+                else if(msg["degree"]=="normol"){
                     //通过控制器跳转界面
                     window.location.href=swiftUrl;
                 }
                 else{
-                    window.location.href=indexUrl;
+                   // window.location.href=indexUrl;
                 }
             }
         })
