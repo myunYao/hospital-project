17b1d2a27c126d700da8e5d242c51a3c65dbb264
myunYao <myunYao@163.com>
2017-11-20 10:00:22 +0800

    修改addmsg.php页面小细节

--- application/views/addmsg.php
+++ application/views/addmsg.php
@@ -26,18 +26,18 @@
 							<ul>
 <!--								<li><span>用户编号：</span><input type="text" value="admin"></li>-->
 								<li><span>医生名字：</span><input class="name" type="text" placeholder="请填写医生名字" value=""></li>
-								<li><span>性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别：</span><input class="sex" type="text" placeholder="性别" value=""></li>
-								<li><span>医生介绍：</span><input class="introduction" type="text" placeholder="请填写医生介绍" value=""></li>
+								<li><span>性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别：</span><input class="sex" type="text" placeholder="请填写医生性别" value=""></li>
+								<li><span>医生介绍：</span><textarea style="width: 200px" class="introduction" type="text" placeholder="请填写医生介绍" value=""></textarea></li>
 								<li><span>医生职位：</span><input class="position" type="text" placeholder="请填写医生职位" value=""></li>
 								<li><span>医生科室：</span><input class="subject" type="text" placeholder="请填写医生所属科目" value=""></li>
-								<li><span>擅长方面：</span><input class="skill" type="text" placeholder="请填写医生擅长方面" value=""></li>
-								<li><span>工作经验：</span><input class="experience" type="text" placeholder="请填写医生工作经验" value=""></li>
+								<li><span>擅长方面：</span><textarea style="width: 200px" class="skill" type="text" placeholder="请填写医生擅长方面" value=""></textarea></li>
+								<li><span>工作经验：</span><textarea style="width: 200px" class="experience" type="text" placeholder="请填写医生工作经验" value=""></textarea></li>
 								<li><span>科室位置：</span><input class="room" type="text" placeholder="请填写医生科室位置" value=""></li>
 <!--								<li><span>用户状态：</span><input class="radio" type="radio" checked="checked"><ol>锁定</ol><input class="radio" type="radio"><ol>正常</ol></li>-->
 								
 							</ul>
 							<div class="btn_box">
-									<input class="queding" id="queding" value="确定" type="button"><input class="cancl" value="取消" type="button">
+									<input class="queding" id="queding" value="确定" type="button"><input class="cancl" id="cancl" value="取消" type="button">
 							</div>
 							
 							
@@ -71,6 +71,7 @@
 		<script>
 			var addmsgUrl = "<?php echo site_url("Doctor/insertDoc")?>"
 			var tiaozhuanUrl = "<?php echo site_url("Office")?>"
+			var refreshUrl="<?php echo site_url("Addmsg")?>"
 		</script>
 		<script src="<?php echo base_url()?>js/addmsg.js"></script>
 	</body>
